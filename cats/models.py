aec2a1197089e3686d49b6a1b1529e863af454d6
Alexey Kazemirenko <xxxalex_93@icloud.com>
2023-10-27 15:40:21 +0300

    123

--- cats/models.py
+++ cats/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 
 
+class Owner(models.Model):
+    first_name = models.CharField(max_length=128)
+    last_name = models.CharField(max_length=128)
+
+    def __str__(self) -> str:
+        return super().__str__()
+
+
 class Cat(models.Model):
     name = models.CharField(max_length=16)
     color = models.CharField(max_length=16)
     birth_year = models.IntegerField()
-
+    owner = models.ForeignKey(
+        Owner, related_name='cats', on_delete=models.CASCADE
+    )
 
     def __str__(self):
         return self.name
