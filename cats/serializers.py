e46e980d6a83c2d2933afaba74a9ed2a567db80c
Alexey Kazemirenko <xxxalex_93@icloud.com>
2023-11-03 12:19:48 +0300

    zzz

--- cats/serializers.py
+++ cats/serializers.py
@@ -1,20 +1,53 @@
 from rest_framework import serializers
-
+import datetime as dt
 from .models import Cat, Owner, Achievement, AchievementCat
+import webcolors
 
 
 class AchievementSerializer(serializers.ModelSerializer):
+    achievement_name = serializers.CharField(source='name')
     class Meta:
         model = Achievement
-        fields = ('id', 'name')
+        fields = ('id', 'achivment_name')
 
+
+class Hex2NameColor(serializers.Field):
+    # При чтении данных ничего не меняем - просто возвращаем как есть
+    def to_representation(self, value):
+        return value
+
+    # При записи код цвета конвертируется в его название
+    def to_internal_value(self, data):
+        # Доверяй, но проверяй
+        try:
+            # Если имя цвета существует, то конвертируем код в название
+            data = webcolors.hex_to_name(data)
+        except ValueError:
+            # Иначе возвращаем ошибку
+            raise serializers.ValidationError('Для этого цвета нет имени')
+        # Возвращаем данные в новом формате
+        return data
+    
 
 class CatSerializer(serializers.ModelSerializer):
     achievements = AchievementSerializer(many=True, required=False)
+    age = serializers.SerializerMethodField()
+    color = Hex2NameColor()
 
     class Meta:
         model = Cat
-        fields = ('id', 'name', 'color', 'birth_year', 'owner', 'achievements')
+        fields = (
+            'id',
+            'name',
+            'color',
+            'birth_year',
+            'owner',
+            'achievements',
+            'age',
+        )
+
+    def get_age(self, obj):
+        return dt.datetime.now().year - obj.birth_year
 
     def create(self, validated_data):
         # Если в исходном запросе не было поля achievements
@@ -31,11 +64,13 @@ class CatSerializer(serializers.ModelSerializer):
         # А потом добавляем его достижения в БД
         for achievement in achievements:
             current_achievement, status = Achievement.objects.get_or_create(
-                **achievement)
+                **achievement
+            )
             # И связываем каждое достижение с этим котиком
             AchievementCat.objects.create(
-                achievement=current_achievement, cat=cat)
-        return cat 
+                achievement=current_achievement, cat=cat
+            )
+        return cat
 
 
 class OwnerSerializer(serializers.ModelSerializer):
@@ -44,3 +79,6 @@ class OwnerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Owner
         fields = ('first_name', 'last_name', 'cats')
+
+
+
